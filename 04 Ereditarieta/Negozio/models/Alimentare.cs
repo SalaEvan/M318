6c0f80e0999adf69f8ff0bb1af9cbe0d7f525d88
SalaEvan <evan.sala@samtrevano.ch>
2024-01-22 11:34:53 +0100

    aggiornamento

---
+++ 04 Ereditarieta/Negozio/models/Alimentare.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negozio.models
+{
+    internal class Alimentare: Prodotto
+    {
+		private DateTime _data;
+
+		public DateTime Data
+		{
+			get { return _data; }
+			set { _data = value; }
+		}
+        public Alimentare(int codice, string descrizione, float prezzo, int giorno, int mese, int anno) : base(codice, descrizione, prezzo)
+        {
+            Codice = codice;
+            Descrizione = descrizione; 
+            Prezzo = prezzo;
+            Data = new DateTime(giorno, mese, anno);
+        }
+    }
+}
---
+++ 04 Ereditarieta/Negozio/models/Prodotto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negozio.models
+{
+    internal class Prodotto
+    {
+		private int _codice;
+
+		public int Codice
+		{
+			get { return _codice; }
+			set { _codice = value; }
+		}
+		private string _descrizione;
+
+		public string Descrizione
+		{
+			get { return _descrizione; }
+			set { _descrizione = value; }
+		}
+		private float _prezzo;
+
+		public float Prezzo
+		{
+			get { return _prezzo; }
+			set { _prezzo = value; }
+		}
+        public Prodotto(int codice, string descrizione, float prezzo)
+        {
+            Codice = codice;
+			Descrizione = descrizione;
+			Prezzo = prezzo;
+        }
+
+        public override string ToString()
+        {
+            return Codice +";" + Descrizione +";"+Prezzo;
+        }
+		public float ApplicaSconto()
+		{
+			return Prezzo + 0.95f;
+		}
+    }
+}
